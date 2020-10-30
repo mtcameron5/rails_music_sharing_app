c6d27eb878f4f76b1f72ef6ca84cf8c0a5708302
mtcameron5 <mtcameron5@gmail.com>
2020-10-30 11:43:22 -0600

    Fixes to stylesheet

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # Returns the hash digest of a given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.const_set
